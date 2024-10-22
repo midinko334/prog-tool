fb8d2b37a28d949da6ed43f7ae4ebfde637e7057
midinko334 <simakaze564@gmail.com>
2024-10-22 11:26:51 +0900

    2024/10/22

---
+++ cui.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+system("sudo systemctl set-default multi-user.target");
+return 1;
+}
---
+++ gui.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+system("sudo systemctl set-default graphical.target");
+return 1;
+}
